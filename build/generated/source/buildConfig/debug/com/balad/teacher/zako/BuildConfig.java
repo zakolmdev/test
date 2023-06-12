666177a6d9bd7c97582ed8ba4516b03378e46d14
zakolmdev <73333027+zakolmdev@users.noreply.github.com>
2023-06-12 20:56:59 +0300

    Add files via upload

---
+++ build/generated/source/buildConfig/debug/com/balad/teacher/zako/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.balad.teacher.zako;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.balad.teacher.zako";
+  public static final String BUILD_TYPE = "debug";
+}
---
+++ src/main/java/com/balad/teacher/zako/ztest.java
@@ -0,0 +1,12 @@
+package com.balad.teacher.zako;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ztest {
+
+    public ztest (Context context){
+        Toast.makeText(context, "رسالة من داخل المكتبة", Toast.LENGTH_SHORT).show();
+    }
+
+}
